c49791cff41df8ea762e7ee749fb2dd97dd51e3a
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-01-11 13:27:40 -0300

    add: import e export de MatchesController

--- app/backend/src/controllers/index.ts
+++ app/backend/src/controllers/index.ts
@@ -1,4 +1,5 @@
 import LoginController from './login.controller';
 import TeamsController from './teams.controller';
+import MatchesController from './matches.controller';
 
-export { LoginController, TeamsController };
+export { LoginController, TeamsController, MatchesController };
