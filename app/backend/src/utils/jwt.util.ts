43d56bdc8e8c3e2f7ebacff30ece3259d91a5f77
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-01-11 00:50:20 -0300

    feat: verificador e decodificador de token jwt

--- app/backend/src/utils/jwt.util.ts
+++ app/backend/src/utils/jwt.util.ts
@@ -1,14 +1,13 @@
 import * as dotenv from 'dotenv';
 import * as jwt from 'jsonwebtoken';
+import HttpException from './http.exception';
 
 dotenv.config();
 
-export default class CreateToken {
+export default class JWT {
   public jwt = jwt;
 
-  public createToken(data: object): string {
-    const payload = { data };
-
+  public createToken(payload: object): string {
     const token = this.jwt.sign(payload, process.env.JWT_SECRET as string, {
       expiresIn: '8h',
       algorithm: 'HS256',
@@ -16,4 +15,20 @@ export default class CreateToken {
 
     return token;
   }
+
+  verifyToken = (token: string): jwt.JwtPayload | string => {
+    try {
+      const payload = jwt.verify(token, process.env.JWT_SECRET as string);
+
+      return payload;
+    } catch (error) {
+      throw new HttpException(401, 'Expired or invalid token');
+    }
+  };
+
+  decoderToken = (token: string): jwt.JwtPayload => {
+    const payload = jwt.decode(token);
+
+    return payload as object;
+  };
 }
