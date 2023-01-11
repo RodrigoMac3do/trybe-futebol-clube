9085967e833d965436beda7e3b1ba2e6403e821d
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-01-11 13:27:50 -0300

    feat: service findAll para matches

---
+++ app/backend/src/services/matches.controller.ts
@@ -0,0 +1,15 @@
+import Teams from '../database/models/TeamsModel';
+import Matches from '../database/models/MatchesModel';
+
+export default class MatchesService {
+  findAll = async () => {
+    const matches = await Matches.findAll({
+      include: [
+        { model: Teams, as: 'teamHome', attributes: { exclude: ['id'] } },
+        { model: Teams, as: 'teamAway', attributes: { exclude: ['id'] } },
+      ],
+    });
+
+    return matches;
+  };
+}
