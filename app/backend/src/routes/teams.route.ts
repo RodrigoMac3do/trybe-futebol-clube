f6db228d47489ed26a1124c7b1d0228398c07732
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-01-11 10:09:09 -0300

    feat: rota de get por id de teams

--- app/backend/src/routes/teams.route.ts
+++ app/backend/src/routes/teams.route.ts
@@ -6,5 +6,6 @@ const router = Router();
 const controller = new TeamsController();
 
 router.get('/', controller.findAll.bind(controller));
+router.get('/:id', controller.findById.bind(controller));
 
 export default router;
