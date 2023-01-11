78178c3204ec601ee71a3105678a8a271b49b5fd
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-01-11 18:38:57 -0300

    add: findAll com query params

--- app/backend/src/controllers/matches.controller.ts
+++ app/backend/src/controllers/matches.controller.ts
@@ -4,9 +4,23 @@ import { MatchesService } from '../services';
 export default class MatchesController {
   public matchesService = new MatchesService();
 
-  findAll = async (_req: Request, res: Response) => {
-    const matches = await this.matchesService.findAll();
+  findAll = async (req: Request, res: Response) => {
+    const {
+      query: { inProgress },
+    } = req;
 
-    res.status(200).json(matches);
+    if (inProgress === 'true') {
+      const progress = await this.matchesService.findAllProgress(true);
+
+      res.status(200).json(progress);
+    } else if (inProgress === 'false') {
+      const progress = await this.matchesService.findAllProgress(false);
+
+      res.status(200).json(progress);
+    } else {
+      const matches = await this.matchesService.findAll();
+
+      res.status(200).json(matches);
+    }
   };
 }
