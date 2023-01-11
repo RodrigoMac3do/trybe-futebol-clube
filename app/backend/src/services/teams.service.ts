f9952376b6a8a47e449c8a943f6254c9f62ab0cc
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-01-11 10:10:34 -0300

    feat: findById de service teams

--- app/backend/src/services/teams.service.ts
+++ app/backend/src/services/teams.service.ts
@@ -1,3 +1,4 @@
+import HttpException from '../utils/http.exception';
 import Teams from '../database/models/TeamsModel';
 
 export default class TeamsService {
@@ -5,5 +6,13 @@ export default class TeamsService {
     const teams = await Teams.findAll();
 
     return teams;
+  };
+
+  findById = async (id: number) => {
+    const team = await Teams.findOne({ where: { id } });
+
+    if (team === null) throw new HttpException(404, 'Time não encontrado');
+
+    return team;
   };
 }
