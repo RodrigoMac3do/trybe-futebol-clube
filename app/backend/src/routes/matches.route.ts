1232eebfd67adc48297780a90225f4380d6f69cc
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-01-16 13:13:30 -0300

    feat: rota patch de /matches

--- app/backend/src/routes/matches.route.ts
+++ app/backend/src/routes/matches.route.ts
@@ -9,4 +9,6 @@ router.get('/', controller.findAll.bind(controller));
 
 router.post('/', controller.create.bind(controller));
 
+router.patch('/:id/finish', controller.update.bind(controller));
+
 export default router;
