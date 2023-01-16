615a8bac8f7e6c6e42a98758650f150a39ce62bb
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-01-16 14:43:41 -0300

    feat: rota patch para atualizar gols

--- app/backend/src/routes/matches.route.ts
+++ app/backend/src/routes/matches.route.ts
@@ -5,10 +5,12 @@ const router = Router();
 
 const controller = new MatchesController();
 
+router.patch('/:id', controller.updateGoals.bind(controller));
+
+router.patch('/:id/finish', controller.update.bind(controller));
+
 router.get('/', controller.findAll.bind(controller));
 
 router.post('/', controller.create.bind(controller));
-
-router.patch('/:id/finish', controller.update.bind(controller));
 
 export default router;
