c146f66559d0456858221c5d1fc6770b90e21350
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-01-17 20:56:15 -0300

    add: import e export de leaderboard

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,5 +1,6 @@
 import loginRoute from './login.route';
 import teamsRoute from './teams.route';
 import matchesRoute from './matches.route';
+import leaderboardRoute from './leaderboard.route';
 
-export { loginRoute, teamsRoute, matchesRoute };
+export { loginRoute, teamsRoute, matchesRoute, leaderboardRoute };
