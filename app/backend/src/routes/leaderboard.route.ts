c987200c5925bd49bc9f1d707caa7944a7886bf1
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-01-17 20:55:13 -0300

    feat: rota de leaderboard/home

---
+++ app/backend/src/routes/leaderboard.route.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import { LeaderboardController } from '../controllers';
+
+const router = Router();
+
+const controller = new LeaderboardController();
+
+router.get('/home', controller.leaderboardHome.bind(controller));
+
+export default router;
