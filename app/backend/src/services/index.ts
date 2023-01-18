07310bb71de9012194833018056ecaee42bf49d9
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-01-18 02:12:56 -0300

    add: import e export de LeaderboardService

--- app/backend/src/services/index.ts
+++ app/backend/src/services/index.ts
@@ -1,5 +1,6 @@
 import LoginService from './login.service';
 import TeamsService from './teams.service';
 import MatchesService from './matches.service';
+import LeaderboardService from './leaderboard.service';
 
-export { LoginService, TeamsService, MatchesService };
+export { LoginService, TeamsService, MatchesService, LeaderboardService };
