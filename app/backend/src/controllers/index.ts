f00b9163ab93d809c118f65557334749f7a678a1
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-01-18 02:07:31 -0300

    add: import e export de LeaderboardController

--- app/backend/src/controllers/index.ts
+++ app/backend/src/controllers/index.ts
@@ -1,5 +1,11 @@
 import LoginController from './login.controller';
 import TeamsController from './teams.controller';
 import MatchesController from './matches.controller';
+import LeaderboardController from './leaderboard.controller';
 
-export { LoginController, TeamsController, MatchesController };
+export {
+  LoginController,
+  TeamsController,
+  MatchesController,
+  LeaderboardController,
+};
