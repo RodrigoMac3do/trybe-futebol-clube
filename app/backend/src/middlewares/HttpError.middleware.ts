b468bdf8a5386993cdd55cda6f8b5f04136166d2
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-02-21 18:23:26 -0300

    refactor: Middleware de HttpError em class

---
+++ app/backend/src/middlewares/HttpError.middleware.ts
@@ -0,0 +1,12 @@
+import { ErrorRequestHandler } from 'express';
+import HttpException from '../utils/HttpException';
+
+export default class HttpError {
+  public static handle: ErrorRequestHandler = (err, _req, res, next) => {
+    const { status, message } = err as HttpException;
+
+    res.status(status || 500).json({ message });
+
+    next();
+  };
+}
