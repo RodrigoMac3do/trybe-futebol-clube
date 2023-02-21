2297d6411b9a3d18c9c1c4b1eaf0e1b096e2aa15
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-02-21 18:26:05 -0300

    refactor: ValidateSchema refatorado para class

--- app/backend/src/services/validations/validationSchema.ts
+++ app/backend/src/services/validations/validationSchema.ts
@@ -1,13 +1,15 @@
 import * as Joi from 'joi';
-import HttpException from '../../utils/http.exception';
+import HttpException from '../../utils/HttpException';
 import { mapStatusCode } from './statusCode';
 
-export default function validateSchema(schema: Joi.Schema, data: object) {
-  const { error, value } = schema.validate(data);
+export default class ValidateSchema {
+  public validate = (schema: Joi.Schema, data: object) => {
+    const { error, value } = schema.validate(data);
 
-  if (error) {
-    throw new HttpException(mapStatusCode(error.message), error.message);
-  }
+    if (error) {
+      throw new HttpException(mapStatusCode(error.message), error.message);
+    }
 
-  return value;
+    return value;
+  };
 }
